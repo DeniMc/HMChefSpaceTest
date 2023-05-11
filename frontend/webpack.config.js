a65e22547d07954c073d68dd6be5dd7cfd505e7d
Denis <denimcardle@gmail.com>
2023-05-11 13:03:52 +0200

    preparing to deploy

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,9 +2,19 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'production',  // Set the mode to 'production'
   entry: './src/scripts/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      // Rule for handling CSS files
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
